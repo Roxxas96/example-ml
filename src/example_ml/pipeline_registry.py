e363c338c032884d2ab7097bda6582c128ae3f81
Alexandre Gomez <gomez.a.corneille@gmail.com>
2025-06-08 16:35:25 +0200

    refactor: namespace personality pipeline set
    Signed-off-by: Alexandre Gomez <gomez.a.corneille@gmail.com>

--- src/example_ml/pipeline_registry.py
+++ src/example_ml/pipeline_registry.py
@@ -1,15 +1,32 @@
 """Project pipelines."""
 
-from kedro.framework.project import find_pipelines
 from kedro.pipeline import Pipeline
+
+from .pipelines.personality import (
+    data_processing as dp,
+)
+from .pipelines.personality import (
+    data_science as ds,
+)
+from .pipelines.personality import (
+    reporting as rp,
+)
 
 
 def register_pipelines() -> dict[str, Pipeline]:
-    """Register the project's pipelines.
+    # Individual pipelines
+    data_processing = dp.create_pipeline()
+    data_science = ds.create_pipeline()
+    reporting = rp.create_pipeline()
 
-    Returns:
-        A mapping from pipeline names to ``Pipeline`` objects.
-    """
-    pipelines = find_pipelines()
-    pipelines["__default__"] = sum(pipelines.values())
-    return pipelines
+    personality = Pipeline(
+        data_processing + data_science + reporting, namespace="personality"
+    )
+
+    return {
+        "personality.data_processing": data_processing,
+        "personality.data_science": data_science,
+        "personality.reporting": reporting,
+        "personality": personality,
+        "__default__": personality,
+    }
